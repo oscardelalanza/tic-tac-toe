f4914d396323afc70332345a8904e71787f8b54a
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-20 15:20:08 -0700

    three new methods added to check all the posible winner casses

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,9 +5,24 @@ class TicTacToe
     X_TOKEN = 'X'
     O_TOKEN = 'O'
     
-    attr_reader :board
-    
     @board = %w[1 2 3 4 5 6 7 8 9]
+    
+    # board rows
+    @row1 = [@board[0], @board[1], @board[2]]
+    @row2 = [@board[3], @board[4], @board[5]]
+    @row3 = [@board[6], @board[7], @board[8]]
+    @rows = [@row1, @row2, @row3]
+    
+    # board columns
+    @col1 = [@board[0], @board[3], @board[6]]
+    @col2 = [@board[1], @board[4], @board[7]]
+    @col3 = [@board[2], @board[5], @board[8]]
+    @cols = [@col1, @col2, @col3]
+    
+    # board diagonals
+    @diag1 = [@board[0], @board[4], @board[8]]
+    @diag2 = [@board[2], @board[4], @board[6]]
+    @diagonals = [@diag1, @diag2]
 
     def display_board
         puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -40,4 +55,33 @@ class TicTacToe
         end
     end
     
+    def win_row
+        winner = false
+        
+        @rows.each do |row|
+            winner = true if row.all?(X_TOKEN) || row.all?(O_TOKEN)
+        end
+        
+        winner
+    end
+    
+    def win_column
+        winner = false
+        
+        @cols.each do |col|
+            winner = true if col.all?(X_TOKEN) || col.all?(O_TOKEN)
+        end
+        
+        winner
+    end
+    
+    def win_diagonal
+        winner = false
+        
+        @diagonals.each do |diag|
+            winner = true if diag.all?(X_TOKEN) || diag.all?(O_TOKEN)
+        end
+        
+        winner
+    end
 end
