7216631f0f82fb8b85232571596fdad13713bfda
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-20 13:14:55 -0700

    rubocop offenses fixed

--- bin/main.rb
+++ bin/main.rb
@@ -8,7 +8,7 @@ player1 = gets.chomp
 puts 'What is the player 2 name?'
 player2 = gets.chomp
 
-puts player1.to_s + "X vs " + player2.to_s + " O "
+puts player1.to_s + ' X vs ' + player2.to_s + ' O '
 time = 0
 
 # Array of board elements
@@ -17,20 +17,18 @@ message = ': Choose the number of the square where you want to place your token'
 
 while time < 9
 
-    # display board showing number of sqaure to choose from
+    # display board showing number of square to choose from
 
     if time.even?
-        puts "#{player1}"+ message
-        player1token = gets.chomp
-
-            # check input to validate whether
+        puts player1.to_s + message
+        # player1token = gets.chomp
+        # check input to validate whether
     else
-    
-        puts "#{player2}" + message
-        player2token = gets.chomp
-
-            # check input to validate whether
+        puts player2.to_s + message
+        # player2token = gets.chomp
+        # check input to validate whether
     end
+    
     time += 1
     puts 'The movement is on board'
 
