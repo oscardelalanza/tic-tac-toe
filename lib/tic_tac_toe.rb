a9ec7c72394ecf452c850fbf42bcd7b0e65cd92a
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-20 14:21:27 -0700

    validate_index method used to check for empty board spaces

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,40 +2,29 @@
 
 class TicTacToe
     
+    X_TOKEN = 'X'
+    O_TOKEN = 'O'
+    
     attr_reader :board
     
-    @@board = %w[1 2 3 4 5 6 7 8 9]
+    @board = %w[1 2 3 4 5 6 7 8 9]
 
     def display_board
-        puts " #{@@board[0]} | #{@@board[1]} | #{@@board[2]} "
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts '-----------'
-        puts " #{@@board[3]} | #{@@board[4]} | #{@@board[5]} "
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
         puts '-----------'
-        puts " #{@@board[6]} | #{@@board[7]} | #{@@board[8]} "
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
     
-    private def validate_index(index)
-        tokens = %w[X O]
-        
-        if
+    private
+    
+    def validate_index(index)
+        if @board[index] == X_TOKEN || @board[index] == O_TOKEN
             false
         else
             true
         end
     end
     
-    def position(postion, player)
-        
-        postion -= 1
-        
-        if player == 1
-            @@board[postion] = 'X'
-        else
-        
-        end
-        
-        
-    end
-        @positon = positon
-
-    end
+end
