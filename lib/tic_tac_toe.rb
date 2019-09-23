5f41dca768f86683e4931c984129341fe8d24a34
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-23 12:23:49 -0700

    if statement open in display_board method to check for winner players

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,11 +67,15 @@ class TicTacToe
     public
     
     def display_board
-        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-        puts '-----------'
-        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-        puts '-----------'
-        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+        if win_column || win_diagonal || win_row
+        
+        else
+            puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+            puts '-----------'
+            puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+            puts '-----------'
+            puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+        end
     end
     
     def set_index(index, player)
