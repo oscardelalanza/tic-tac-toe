07737d9b3ae42c7e729529195226d473b1c23210
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-25 15:48:42 -0700

    attr_readers added to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Player
+    
+    attr_reader :name
+    attr_reader :player_num
+    
     def initialize(name, player_num)
         @name = name
         @player_num = player_num
