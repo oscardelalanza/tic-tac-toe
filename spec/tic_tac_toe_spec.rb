acd86d122397d97f83c1c2673da48f8cca3abbb4
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-09-27 15:40:29 -0700

    TicTacToe test cases added

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
 require_relative '../lib/tic_tac_toe'
-require_relative '../lib/board'
 
 RSpec.describe TicTacToe do
     context '#winner' do
-      it 'returns an integer if board has a winner combination' do
-        expect(winner(board)).to be a(integer)
-      end
+        subject(:game) { TicTacToe.new('player1', 'player2') }
+
+        it 'returns nil if board has not a winner combination' do
+            expect(game.winner(%w[1 2 3 4 5 6 7 8 9])).to eql(nil)
+        end
+
+        it 'returns 1 if the board has a winner combination for player 1 on a row' do
+            expect(game.winner([:X, :X, :X, '4', '5', '6', '7', '8', '9'])).to eql(1)
+        end
+
+        it 'returns 1 if the board has a winner combination for player 1 on a column' do
+            expect(game.winner([:X, '2', '3', :X, '5', '6', :X, '8', '9'])).to eql(1)
+        end
+
+        it 'returns 1 if the board has a winner combination for player 1 on a diagonal' do
+            expect(game.winner([:X, '2', '3', '4', :X, '6', '7', '8', :X])).to eql(1)
+        end
+
+        it 'returns 2 if the board has a winner combination for player 2 on a row' do
+            expect(game.winner(['1', '2', '3', :O, :O, :O, '7', '8', '9'])).to eql(2)
+        end
+
+        it 'returns 2 if the board has a winner combination for player 2 on a column' do
+            expect(game.winner(['1', :O, '3', '4', :O, '6', '7', :O, '9'])).to eql(2)
+        end
+
+        it 'returns 2 if the board has a winner combination for player 2 on a diagonal' do
+            expect(game.winner(['1', '2', :O, '4', :O, '6', :O, '8', '9'])).to eql(2)
+        end
     end
 end
